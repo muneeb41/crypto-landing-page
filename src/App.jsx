6a90648d0a775dccb132487427593e64d35b0121
muneeb41 <maxm26246@gmail.com>
2025-03-14 15:30:11 +0530

    deploy hash url

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing/Landing.jsx";
 import Success from "./pages/success/Success.jsx";
 
 function App() {
   return (
-    <Router basename="/crypto-landing-page">
+    <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/crypto-landing-page/success" element={<Success />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </Router>
   );
