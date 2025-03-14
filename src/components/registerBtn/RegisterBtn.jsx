89eec65c4ab05224e4d4e7928da201539687f395
muneeb41 <maxm26246@gmail.com>
2025-03-14 15:15:55 +0530

    deploy anchor to link

--- src/components/registerBtn/RegisterBtn.jsx
+++ src/components/registerBtn/RegisterBtn.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { paymentUrl } from '../../utils/const.js'
+import { Link } from 'react-router-dom'
+
 
 const RegisterBtn = () => {
   return (
     <div className='flex justify-center my-5'>
-        <a href={paymentUrl} target='_blank'>
-            <button className='bg-green-600 box-shadow-main font-bold hover-scale py-3 mx-3 text-xl text-white rounded-lg px-1 animate-move sm:px-3 sm:text-2xl'>Join The Workshop At Just Rs. 9/-</button>
-        </a>
+
+       <Link to='/success'>
+       <button className='bg-green-600 box-shadow-main font-bold hover-scale py-3 mx-3 text-xl text-white rounded-lg px-1 animate-move sm:px-3 sm:text-2xl'>Join The Workshop At Just Rs. 9/-</button>
+       </Link>
+       
     </div>
   )
 }
